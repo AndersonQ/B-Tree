26be69d371156055d55b1986b3ae955af3fc7d8d
Anderson de França Queiroz <contato@andersonq.eti.br>
2012-11-07 09:08:50 -0200

    Created main methods

--- src/b/tree/BTree.java
+++ src/b/tree/BTree.java
@@ -1,5 +1,51 @@
 package b.tree;
 
 public class BTree {
+	
+	/** The root of this tree */
+	Page root;
+	
+	public BTree()
+	{
+		
+	}
+	
+	/**
+	 * Find a page
+	 * @param key a key to find
+	 * @param pt the page where is the key
+	 * @param f a boolean to indicate success or fail
+	 * @param pos the position of the key in the page
+	 */
+	public void findB(int key, Page pt, boolean f, int pos)
+	{
+		
+	}
+	
+	/** 
+	 * Insert a registry in the tree
+	 * @param reg a registry to insert
+	 */
+	public void insertB(Registry reg)
+	{
+		
+	}
+	
+	/**
+	 * Remove a registry
+	 * @param reg a registry to remove
+	 */
+	public void removeReg(Registry reg)
+	{
+		
+	}
+	
+	/**
+	 * List all elements in-order
+	 */
+	public void listInOrder()
+	{
+		
+	}
 
 }
