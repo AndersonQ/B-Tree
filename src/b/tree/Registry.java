4c480e1535966a4030d13d1c6a74c31c841952b3
Anderson de França Queiroz <contato@andersonq.eti.br>
2012-11-07 10:11:57 -0200

    Get and Set

--- src/b/tree/Registry.java
+++ src/b/tree/Registry.java
@@ -9,6 +9,17 @@ public class Registry {
 		this.key = key;
 	}
 	
+	public int getKey() {
+		return key;
+	}
+
+	public void setKey(int key) {
+		this.key = key;
+	}
+
+	/**
+	 * List all elements
+	 */
 	public void listElements()
 	{
 		
