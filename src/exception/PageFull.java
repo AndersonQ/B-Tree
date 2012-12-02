1fe1bab41e75d35fb89f3ee3a974751ff191a7c1
Anderson de França Queiroz <contato@andersonq.eti.br>
2012-12-02 19:37:30 -0200

    Exception pageFull created

---
+++ src/exception/PageFull.java
@@ -0,0 +1,19 @@
+package exception;
+
+public class PageFull extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public PageFull()
+	{
+		super();
+	}
+	
+	public PageFull(String s)
+	{
+		super(s);
+	}
+}
