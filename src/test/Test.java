956ec073a20123641bfc6187db73e478cfd6f1bb
Anderson de França Queiroz <contato@andersonq.eti.br>
2012-12-05 17:50:36 -0200

    New tests

--- src/test/Test.java
+++ src/test/Test.java
@@ -13,18 +13,22 @@ public class Test {
 		BTree btree = new BTree(2);
 		
 		Registry ret;
-		Registry[] regs = new Registry [5];
+		Registry[] regs = new Registry [15];
+		Registry r30, r29;
 		
-		int j = regs.length;
+		r30 = new Registry(30);
+		r29 = new Registry(29);
+		
+		//int j = regs.length;
 		for (int i = 0; i < regs.length; i++)
 		{
-			regs[i] = new Registry((j)*10);
-			j--;
+			regs[i] = new Registry((i+1));
+			//j--;
 		}
 		
 		for (Registry r: regs)
 		{
-			System.out.print("Adding reg " + r);
+			System.out.println("Adding reg " + r);
 			try
 			{
 				btree.insert(r);
@@ -36,13 +40,23 @@ public class Test {
 			}
 		}
 		
+		btree.insert(r30);
+		btree.insert(r29);
+		
 		System.out.println();
 		for (Registry r: regs)
 		{
 			System.out.println("Searching reg " + r);
 			ret = btree.find(r);
 			System.out.println("Search result:  "+ ret);
+			System.out.println();
 		}
+		
+		System.out.println("Searching reg " + r30);
+		ret = btree.find(r30);
+		System.out.println("Search result:  "+ ret);
+		System.out.println();
+		
+		System.out.println("\nPrinting B-Tree...\n" + btree.toString());
 	}
-
 }
